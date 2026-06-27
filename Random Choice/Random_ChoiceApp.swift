600f448ea8216013b657519eea502b033a87bf60
TheDemonLord333 <thedemonlord333@gmail.com>
2026-06-27 19:18:17 +0200

    Initial Commit

---
+++ Random Choice/Random_ChoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_ChoiceApp.swift
+//  Random Choice
+//
+//  Created by David Martens on 27.06.26.
+//
+
+import SwiftUI
+
+@main
+struct Random_ChoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
